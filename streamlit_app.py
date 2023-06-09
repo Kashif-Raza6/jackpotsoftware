c6311b7931959de00abd32a64255ae16b287d89d
Kashif-Raza6 <96944736+Kashif-Raza6@users.noreply.github.com>
2023-06-09 18:23:32 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 from langchain.schema import HumanMessage
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.chat_models import ChatOpenAI
+from selenium_solution import selenium_scrape
+from serpapi_solution import serpapi_scrape
+import pandas as pd
 load_dotenv()
 
 st.markdown("""
@@ -61,17 +64,40 @@ if uploaded_file is not None:
     
     with st.spinner('Extracting the information...'):
         output = chat_model([HumanMessage(content=prompt)])
+        st.write(output.content)
 
-    from langchain.utilities import GoogleSerperAPIWrapper
-    search = GoogleSerperAPIWrapper()
+    # from langchain.utilities import GoogleSerperAPIWrapper
+    # search = GoogleSerperAPIWrapper()
 
-    try:
-        with st.spinner('Searching for jobs...'):
-            search_result = search.run(f"{output.content} Jobs in {job_location}")
-        st.success('Search completed!')
-    except Exception as e:
-        st.error(f'Error: {e}')
+    # try:
+    #     with st.spinner('Searching for jobs...'):
+    #         search_result = search.run(f"Web developer Jobs in USA")
+    #     st.success('Search completed!')
+    # except Exception as e:
+    #     st.error(f'Error: {e}')
 
     st.write('------------------------')
     st.header('Search Result')
-    st.write(search_result)
+    st.write('------------------------')
+
+    with st.spinner('Extracting the Jobs...'):
+        data = selenium_scrape(output.content)
+        # Now finding the total number of jobs scraped
+        st.write("Total number of jobs found:", len(data))
+        # Now checking the url column has any null values
+        st.write("Total number of which has no URL in  column:", data['url'].isnull().sum())
+        # Now checking the description column has any null values
+        st.write("Total number of jobs which has no description given:", data['description'].isnull().sum())
+        # Removing the rows with null values in url column
+        data1 = data.dropna(subset=['url'])
+        # Now check the total number of rows after removing the null values
+        st.write("Total number of jobs after removing the null values:", len(data1))
+
+
+    df = data1[['title', 'company', 'location', 'url']]
+
+    # Modify the URLs in the DataFrame to create clickable links
+    df['url'] = df['url'].apply(lambda x: f'<a target="_blank" href="{x}">{x}</a>')
+
+    # Display the DataFrame in Streamlit
+    st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
